dae1a9e4bdcb2763890bec93497bb9f0f8a71b03
Naveen <npn.npntraining@gmail.com>
2022-01-07 12:42:21 +0530

    Code Commit

--- tests/test_sample_app.py
+++ tests/test_sample_app.py
@@ -5,17 +5,36 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-def test_simple():
+def test_1():
     driver = webdriver.Chrome(webdriver_manager.chrome.ChromeDriverManager().install())
     driver.maximize_window()
     driver.implicitly_wait(10)
     driver.get('http://localhost:8000/aut-devops/SampleApp.html')
     terms = driver.find_element(By.ID, 'terms').text
-    print(terms)
     next_btn = driver.find_element(By.ID, 'btn')
-    time.sleep(5)
     assert (next_btn.is_enabled() is False)
+    driver.close()
+
+def test_2():
+    driver = webdriver.Chrome(webdriver_manager.chrome.ChromeDriverManager().install())
+    driver.maximize_window()
+    driver.implicitly_wait(10)
+    driver.get('http://localhost:8000/aut-devops/SampleApp.html')
+    terms = driver.find_element(By.ID, 'terms').text
+    next_btn = driver.find_element(By.ID, 'btn')
     driver.find_element(By.ID, 'agree').click()
-    time.sleep(5)
     next_btn_afterclick = driver.find_element(By.ID, 'btn')
     assert (next_btn_afterclick.is_enabled() is True)
+
+
+def test_3():
+    driver = webdriver.Chrome(webdriver_manager.chrome.ChromeDriverManager().install())
+    driver.maximize_window()
+    driver.implicitly_wait(10)
+    driver.get('http://localhost:8000/aut-devops/SampleApp.html')
+    terms = driver.find_element(By.ID, 'terms').text
+    next_btn = driver.find_element(By.ID, 'btn')
+    driver.find_element(By.ID, 'agree').click()
+    driver.find_element(By.ID, 'agree').click()
+    next_btn_afterclick = driver.find_element(By.ID, 'btn')
+    assert (next_btn_afterclick.is_enabled() is False)
